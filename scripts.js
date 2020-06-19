f3c16d9afedd495388958c246a81b2ddad61c529
August12345 <153925080@qq.com>
2020-06-19 11:05:28 +0800

    第二次提交

--- scripts.js
+++ scripts.js
@@ -42,3 +42,47 @@ function renderEditor() {
 
     };
 }
+
+function renderTaskItems() {
+    console.log("render items");
+    let itemsEl = document.querySelector("#default-todo-panel .todo-items");
+
+    itemsEl.querySelectorAll("div").forEach((node) => node.remove());
+
+
+
+    for (let i = 0; i < tasks.length; i++) {
+        let task = tasks[i];
+        let itemEl = document.createElement("div");
+        itemEl.className = "task";
+
+        let doneEl = document.createElement("input");
+        doneEl.type = "checkbox";
+        doneEl.checked = task.done;
+        if (task.done) {
+            itemEl.classList.add("done");
+        } else {
+            itemEl.classList.remove("done");
+        }
+        doneEl.onchange = (e) => {
+            task.done = e.target.checked;
+            if (task.done) {
+                itemEl.classList.add("done");
+            } else {
+                itemEl.classList.remove("done");
+            }
+        }
+        itemEl.append(doneEl);
+
+        let titleEl = document.createElement("label");
+        titleEl.innerText = task.title;
+        itemEl.append(titleEl);
+
+        let ctrlbarEl = renderTaskCtrlBar(tasks,i);
+
+        itemEl.append(ctrlbarEl);
+
+        itemsEl.append(itemEl);
+    }
+}
+
